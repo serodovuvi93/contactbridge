42d854c37fa70482d08c035c6af8305924171a06
serodovuvi93 <serodovuvi93@gmail.com>
2026-01-15 09:48:01 -0500

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/contactbridge/',
   build: {
     // Prevent emptying output dir between steps
     emptyOutDir: false, 
